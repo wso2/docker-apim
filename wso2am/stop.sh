97e676e0f7e5daf65255e4b6ebefe65ecbeaa89f
anuruddhal <anuruddhal@wso2.com>
2016-08-11 18:57:07 +0530

    Adding wso2am dockerfiles

---
+++ wso2am/stop.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# ------------------------------------------------------------------------
+#
+# Copyright 2016 WSO2, Inc. (http://wso2.com)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License
+
+# ------------------------------------------------------------------------
+set -e
+
+product_name=wso2am
+
+prgdir=$(dirname "$0")
+script_path=$(cd "$prgdir"; pwd)
+common_folder=$(cd "${script_path}/../common/scripts/"; pwd)
+
+bash ${common_folder}/docker-stop.sh -n ${product_name}
