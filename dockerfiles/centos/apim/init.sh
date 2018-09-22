4f96fd2a164a1ec5567295a3c6c96b4ad7de5641
chirangaalwis <chirangaalwis@gmail.com>
2018-09-22 14:45:06 +0530

    Improve Alpine based Docker resources

--- dockerfiles/centos/apim/init.sh
+++ dockerfiles/centos/apim/init.sh
@@ -67,7 +67,7 @@ test ! -d ${WORKING_DIRECTORY} && echo "WSO2 Docker non-root user home does not 
 test ! -d ${WSO2_SERVER_HOME} && echo "WSO2 Docker product home does not exist" && exit 1
 
 # if a deployment_volume is present and empty, copy original deployment artifacts to server...
-# copying original artifacts to ${WORKING_DIRECTORY}/wso2-tmp/deployment was already done in the Dockerfile
+# copying original artifacts to ${WORKING_DIRECTORY}/wso2-tmp/server was already done in the Dockerfile
 # these artifacts will be copied to deployment_volume if it is empty, before the server is started
 if test -d ${original_deployment_artifacts}; then
     if [ -z "$(ls -A ${deployment_volume}/)" ]; then
