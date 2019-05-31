00c589912c70535b2b3c142e88ff0ccb38cb35f5
Chiranga Alwis <chirangaalwis@gmail.com>
2019-05-31 15:48:18 +0530

    Remove temporarily persisted, default content of persistent runtime artifact folders in Ubuntu based WSO2 APIM Dockerfile

--- dockerfiles/ubuntu/apim/init.sh
+++ dockerfiles/ubuntu/apim/init.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # ------------------------------------------------------------------------
-# Copyright 2018 WSO2, Inc. (http://wso2.com)
+# Copyright 2019 WSO2, Inc. (http://wso2.com)
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -20,12 +20,6 @@ set -e
 # volume mounts
 config_volume=${WORKING_DIRECTORY}/wso2-config-volume
 artifact_volume=${WORKING_DIRECTORY}/wso2-artifact-volume
-deployment_volume=${WSO2_SERVER_HOME}/repository/deployment/server
-# original deployment artifacts
-original_deployment_artifacts=${WORKING_DIRECTORY}/wso2-tmp/server
-
-# capture Docker container IP from the container's /etc/hosts file
-docker_container_ip=$(awk 'END{print $1}' /etc/hosts)
 
 # check if the WSO2 non-root user home exists
 test ! -d ${WORKING_DIRECTORY} && echo "WSO2 Docker non-root user home does not exist" && exit 1
@@ -33,25 +27,10 @@ test ! -d ${WORKING_DIRECTORY} && echo "WSO2 Docker non-root user home does not 
 # check if the WSO2 product home exists
 test ! -d ${WSO2_SERVER_HOME} && echo "WSO2 Docker product home does not exist" && exit 1
 
-# if a deployment_volume is present and empty, copy original deployment artifacts to server...
-# copying original artifacts to ${WORKING_DIRECTORY}/wso2-tmp/server was already done in the Dockerfile
-# these artifacts will be copied to deployment_volume if it is empty, before the server is started
-if test -d ${original_deployment_artifacts}; then
-    if [ -z "$(ls -A ${deployment_volume}/)" ]; then
-	    # if no artifact is found under <WSO2_SERVER_HOME>/repository/deployment/server; copy originals
-	    echo "Copying original deployment artifacts from temporary location to server..."
-	    cp -R ${original_deployment_artifacts}/* ${deployment_volume}/
-    fi
-fi
-
 # copy any configuration changes mounted to config_volume
-test -d ${config_volume}/ && cp -RL ${config_volume}/* ${WSO2_SERVER_HOME}/
+test -e ${config_volume}/ && cp -RL ${config_volume}/* ${WSO2_SERVER_HOME}/
 # copy any artifact changes mounted to artifact_volume
-test -d ${artifact_volume}/ && cp -RL ${artifact_volume}/* ${WSO2_SERVER_HOME}/
-
-# make any node specific configuration changes
-# for example, set the Docker container IP as the `localMemberHost` under axis2.xml clustering configurations (effective only when clustering is enabled)
-sed -i "s#<parameter\ name=\"localMemberHost\".*<\/parameter>#<parameter\ name=\"localMemberHost\">${docker_container_ip}<\/parameter>#" ${WSO2_SERVER_HOME}/repository/conf/axis2/axis2.xml
+test -e ${artifact_volume}/ && cp -RL ${artifact_volume}/* ${WSO2_SERVER_HOME}/
 
 # start WSO2 Carbon server
 sh ${WSO2_SERVER_HOME}/bin/wso2server.sh "$@"
