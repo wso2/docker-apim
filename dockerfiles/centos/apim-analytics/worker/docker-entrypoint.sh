72697132a0986505208bcd7a053a7409a79fe6c7
Chiranga Alwis <chirangaalwis@gmail.com>
2019-06-06 00:39:10 +0530

    Remove unnecessary * from content check of indirect mounts

--- dockerfiles/centos/apim-analytics/worker/docker-entrypoint.sh
+++ dockerfiles/centos/apim-analytics/worker/docker-entrypoint.sh
@@ -28,9 +28,9 @@ test ! -d ${WORKING_DIRECTORY} && echo "WSO2 Docker non-root user home does not 
 test ! -d ${WSO2_SERVER_HOME} && echo "WSO2 Docker product home does not exist" && exit 1
 
 # copy any configuration changes mounted to config_volume
-test -d ${config_volume} && [ "$(ls -A ${config_volume}/*)" ] && cp -RL ${config_volume}/* ${WSO2_SERVER_HOME}/
+test -d ${config_volume} && [ "$(ls -A ${config_volume})" ] && cp -RL ${config_volume}/* ${WSO2_SERVER_HOME}/
 # copy any artifact changes mounted to artifact_volume
-test -d ${artifact_volume} && [ "$(ls -A ${artifact_volume}/*)" ] && cp -RL ${artifact_volume}/* ${WSO2_SERVER_HOME}/
+test -d ${artifact_volume} && [ "$(ls -A ${artifact_volume})" ] && cp -RL ${artifact_volume}/* ${WSO2_SERVER_HOME}/
 
 # start WSO2 Carbon server
 sh ${WSO2_SERVER_HOME}/bin/worker.sh "$@"
