a44f151517f13f8484080630c1b231282b40bb6d
Chiranga Alwis <chirangaalwis@gmail.com>
2019-08-21 19:57:26 +0530

    Add support for artifact synchronization between API Manager nodes

--- dockerfiles/ubuntu/apim/docker-entrypoint.sh
+++ dockerfiles/ubuntu/apim/docker-entrypoint.sh
@@ -20,12 +20,35 @@ set -e
 # volume mounts
 config_volume=${WORKING_DIRECTORY}/wso2-config-volume
 artifact_volume=${WORKING_DIRECTORY}/wso2-artifact-volume
+# home of the directories to be artifact synced within the WSO2 product home
+deployment_volume=${WSO2_SERVER_HOME}/repository/deployment/server
+# home of the directories with preserved, default deployment artifacts
+original_deployment_artifacts=${WORKING_DIRECTORY}/wso2-tmp
 
 # check if the WSO2 non-root user home exists
 test ! -d ${WORKING_DIRECTORY} && echo "WSO2 Docker non-root user home does not exist" && exit 1
 
 # check if the WSO2 product home exists
 test ! -d ${WSO2_SERVER_HOME} && echo "WSO2 Docker product home does not exist" && exit 1
+
+# shared artifact directories
+set "executionplans" "synapse-configs"
+# if the original directory locations of artifacts to be synced between nodes are empty,
+# copy the preserved, default content of these folders to these original locations
+for shared_directory
+do
+  if test -d ${original_deployment_artifacts}/${shared_directory};
+  then
+    if [ -z "$(ls -A ${deployment_volume}/${shared_directory})" ]; then
+      if ! cp -R ${original_deployment_artifacts}/${shared_directory}/* ${deployment_volume}/${shared_directory};
+      then
+        echo "Failed to copy the preserved, default artifacts to original location (${deployment_volume}/${shared_directory})"
+        exit 1
+      fi
+      echo "Successfully copied the preserved, default artifacts to original location (${deployment_volume}/${shared_directory})"
+    fi
+  fi
+done
 
 # copy any configuration changes mounted to config_volume
 test -d ${config_volume} && [ "$(ls -A ${config_volume})" ] && cp -RL ${config_volume}/* ${WSO2_SERVER_HOME}/
