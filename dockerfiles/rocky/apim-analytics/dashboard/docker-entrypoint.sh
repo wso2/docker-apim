93061a935546ba42d05f653638bdf5c8b3d403ee
pasant9 <pasan96tennakoon@gmail.com>
2024-09-24 12:39:39 +0530

    Fix license header

--- dockerfiles/rocky/apim-analytics/dashboard/docker-entrypoint.sh
+++ dockerfiles/rocky/apim-analytics/dashboard/docker-entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # ------------------------------------------------------------------------
-# Copyright 2018 WSO2, Inc. (http://wso2.com)
+#
+# Copyright 2024 WSO2, LLC. (http://wso2.com)
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,6 +14,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License
+#
 # ------------------------------------------------------------------------
 
 set -e
